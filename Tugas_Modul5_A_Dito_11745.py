725408134b258bde8fd2b6de4545991083ccfc1a
RaditoPutra <220711745@students.uajy.ac.id>
2024-11-21 23:01:26 +0700

    Update Tugas_Modul5_A_Dito_11745.py

--- Tugas_Modul5_A_Dito_11745.py
+++ Tugas_Modul5_A_Dito_11745.py
@@ -4,9 +4,7 @@ import pickle
 from PIL import Image
 import os
 
-# Definisikan jalur model
-model_directory = r'D:\Tugas\Sem 5\Mesin Learning\Introduction to Deep Learning (Praktek)\Introduction to Deep Learning (Praktek)'
-model_path = os.path.join(model_directory, r"best_model.pkl")
+model_path = r'best_model.pkl'
 
 if os.path.exists(model_path):
     try:
